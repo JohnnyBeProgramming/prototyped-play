cece5366d63ae83ea6f6cee3c4808e2466a16b56
JohnnyBeProgramming <Johnathan.Lodder@Gmail.com>
2015-06-13 16:44:31 +0200

    Updating template styling

---
+++ Static HTML Templates/assets/lib/app.js
@@ -0,0 +1,14 @@
+﻿angular.module('myApp', [
+])
+    .controller('MainViewController', function ($rootScope) {
+        this.expanded = false;
+        this.show = function (link) {
+            this.expanded = true;
+        }
+        this.import = function (link) {
+            this.expanded = false;
+        }
+    })
+    .run(function () {
+        console.log(' - Application Started: ', Date.now().toString());
+    })
