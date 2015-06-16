7e985a0cf05be0ce8c6b8257b03c9eff0a32b2d4
JohnnyBeProgramming <Johnathan.Lodder@Gmail.com>
2015-06-16 17:40:15 +0200

    Updated Angular components

--- Static HTML Templates/assets/lib/app.js
+++ Static HTML Templates/assets/lib/app.js
@@ -1,5 +1,70 @@
 ﻿angular.module('myApp', [
 ])
+    .directive('dockedContainer', function () {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/container.tpl.html'
+        };
+    })
+    .directive('dockedIcon', function () {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/icon.tpl.html'
+        };
+    })
+    .directive('dockedTop', function () {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/top.tpl.html'
+        };
+    })
+    .directive('dockedTopLeft', function() {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/topLeft.tpl.html'
+        };
+    })
+    .directive('dockedTopRight', function() {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/topRight.tpl.html'
+        };
+    })
+    .directive('dockedLeftNav', function () {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/left.tpl.html'
+        };
+    })
+    .directive('dockedMain', function () {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/main.tpl.html'
+        };
+    })
+    .directive('dockedFooter', function () {
+        return {
+            restrict: 'AEM',
+            replace: true,
+            transclude: true,
+            templateUrl: 'views/common/docked/footer.tpl.html'
+        };
+    })
+
     .run(function () {
         console.log(' - Application Started: ', (new Date()).toString());
     })
