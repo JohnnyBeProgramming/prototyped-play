181a9aea1b53bebcb48269f7cbd4dcf3d53d3f43
JohnnyBeProgramming <Johnathan.Lodder@Gmail.com>
2015-07-10 13:16:07 +0200

    Refactoring code

---
+++ Static HTML Templates/app/assets/lib/app.start.js
@@ -0,0 +1,65 @@
+﻿var appLoader = {
+    counter: 0,
+    init: function (deps, callback, errorHandler) {
+        appLoader.counter = 0;
+        appLoader.failures = 0;
+        var requirements = [];
+        for (var name in deps) {
+            var dep = deps[name];
+            if (!('qry' in dep) || dep.qry) {
+                requirements.push(dep.url);
+            }
+        }
+
+        if (requirements.length) {
+            console.log(' - Loading dependencies');
+            requirements.forEach(function (url) {
+                console.log('   + ', url);
+                appLoader.counter++;
+                appLoader.load(url, function (result) {
+                    if (!result) {
+                        console.warn('Warning: Script timed out:', url);
+                        appLoader.failures++;
+                    } else {
+                        // Loaded...
+                    }
+                    appLoader.counter--;
+                    appLoader.check(callback, errorHandler);
+                });
+            });
+        } else {
+            appLoader.check(callback, errorHandler);
+        }
+    },
+
+    load: function (url, callback) {
+        var hasLoaded = false;
+        var srciptElem = document.createElement('script');
+        if (srciptElem) {
+            srciptElem.onload = function (evt) {
+                hasLoaded = true;
+                if (callback) callback(url, evt);
+            }
+            srciptElem.src = url;
+            document.body.appendChild(srciptElem);
+        }
+        var intv = setInterval(function () {
+            clearInterval(intv);
+            if (!hasLoaded && callback) {
+                callback(false, null);
+            }
+        }, 2 * 60 * 1000);
+    },
+
+    check: function (callback, errorHandler) {
+        if (appLoader.counter == 0) {
+            if (appLoader.failures > 0) {
+                var err = new Error('Failed to load one or more dependencies...');
+                if (errorHandler) errorHandler(err);
+                throw err;
+            } else {
+                if (callback) callback();
+            }
+        }
+    },
+};
--- Static HTML Templates/build/Brocfile.js
+++ Static HTML Templates/build/Brocfile.js
@@ -5,20 +5,24 @@ var mergeTrees = require('broccoli-merge-trees');
 var ngTemplCache = require('broccoli-ng-templatecache');
 var uglifyJs = require('broccoli-uglify-js');
 var stew = require('broccoli-stew');
-var app = '../';
+var path = require('path');
+var base = '../';
+var dest = '../../../dest';
 
 /** 
  * move the index.html file from the project /app folder
  * into the build production folder
  */
-var appHtml = pickFiles(app, {
-    srcDir: './',
+var appHtml = pickFiles(base, {
+    srcDir: './app',
     files: [
+        '**/*.js',
+        '**/*.css',
         '**/*.html',
     ],
-    destDir: '/'
+    destDir: dest,
 });
-
+/*
 // Generate ng Templates
 var templates = new ngTemplCache([appHtml], {
     module: 'myApp.templates',
@@ -26,42 +30,48 @@ var templates = new ngTemplCache([appHtml], {
     standalone: true,
     prefix: './',
 });
-console.log(' - templates: ', templates);
-
+*/
 /*
 var appCss = compileLess(app, 'assets/css/app.less', 'assets/css/app.css', {
     paths: ['/assets/css']
 });
  */
-var appCss = pickFiles(app, {
-    srcDir: '/assets/css/',
+var appCss = pickFiles(base, {
+    srcDir: './app/assets/css/',
     files: [
-        '**/*.css',
+//        '**/*.css',
     ],
-    destDir: '/assets/css/'
+    destDir: path.join(dest, 'assets/css/'),
 })
 
 /**
  * concatenate and compress all of our JavaScript files in 
  * the project /app folder into a single app.js file in 
  * the build production folder
- */
 var appJs = concatenate(app, {
     inputFiles: [
         'assets/lib/vend/jquery/jquery-2.1.3.min.js',
-        'assets/lib/vend/angular/angular.js',
+        'assets/lib/vend/angular/angular.min.js',
         'assets/lib/vend/angular/angular-animate.js',
         'assets/lib/app.js',
     ],
-    outputFile: '/assets/lib/app.js',
-    header: '/** Dont feed the trolls... **/'
+    outputFile: path.join(process.cwd(), dest, 'assets/lib/vendor.js'),
 });
+ */
+var appJs = pickFiles(base, {
+    srcDir: './app/assets/lib/',
+    files: [
+//        '**/*.js',
+    ],
+    destDir: path.join(dest, 'assets/lib/'),
+})
+
 /*
 appJs = uglifyJs(appJs, {
     compress: true
 });
 */
 // merge HTML, JavaScript and CSS trees into a single tree and export it
-var appTree = mergeTrees([appHtml, appJs, appCss, templates]);
+var appTree = mergeTrees([appHtml, appCss, appJs/*, templates*/]);
 module.exports = appTree;
 //module.exports = stew.rm(appTree, '/build/**');
